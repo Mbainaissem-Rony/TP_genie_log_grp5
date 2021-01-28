d0e88cf1c693dab7c1bf135f21943d1321dcea80
Mbainaissem-Rony <77842609+Mbainaissem-Rony@users.noreply.github.com>
2021-01-28 14:06:36 +0100

    Update ClasseMain.java

--- Gestion_de ville/src/ClasseMain.java
+++ Gestion_de ville/src/ClasseMain.java
@@ -3,14 +3,14 @@ public class ClasseMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Capitale yaoundé = new Capitale();
+Capitale yaoundÃ© = new Capitale();
 Ville ville = new Ville();
-Ville ville1 = new Ville ("Ngoundéré", "Cameroun", 19000000);
+Ville ville1 = new Ville ("NgoundÃ©rÃ©", "Cameroun", 19000000);
 Ville ville2 = new Ville("Ndjamena", "Tchad", 25000000);
 System.out.println(" \n ville = " +ville.getNom() + " est une ville du " + ville.getPays() + " de " + ville.getHabitant()+" habitants");
 System.out.println("\n ville1 = " +ville1.getNom()+ " est une ville du " + ville1.getPays() + " de " +ville1.getHabitant()+ " habitants");
 System.out.println("\n ville2 = " +ville2.getNom()+ " est une ville du " + ville2.getPays() + " de " +ville2.getHabitant()+ " habitants");
-// échangeons les données des villes 1 et 2 par l'intermediaire de l'objet temps
+// Ã©changeons les donnÃ©es des villes 1 et 2 par l'intermediaire de l'objet temps
 Ville temps = new Ville();
 temps = ville1;
 ville1 = ville2;
@@ -30,9 +30,10 @@ System.out.println(ville.decriToi());
 System.out.println(ville1.decriToi());
 System.out.println(ville2.decriToi());
 System.out.println(ville2.comparer(ville1));
-System.out.println("Le nombre d'instances de la classe Ville est : " + ville1.nombreInstance);
-System.out.println("Le nombre d'instances de la classe Ville est : " + ville1.getnombreInstacesBiss());
-System.out.println(yaoundé.decriToi());
+System.out.println("Le nombre d'instances de la classe Ville est : " + ville1.nombreInstance);   
+System.out.println("Le nombre d'instances de la classe Ville est : " + ville1.getnombreInstacesBiss());                        
+System.out.println(yaoundÃ©.decriToi());
 	}
 		}
+
 
